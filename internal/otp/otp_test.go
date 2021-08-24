efd6147eb971637de556f26d7ac7d1d55f95dfb1
lauslim12 <nicholasdwiarto@yahoo.com>
2021-08-24 14:31:04 +0700

    test: Add tests for OTP verification

--- internal/otp/otp_test.go
+++ internal/otp/otp_test.go
@@ -55,6 +55,8 @@ func TestPad(t *testing.T) {
 }
 
 func TestGenerate(t *testing.T) {
+	sharedSecret := toBase32("The quick brown fox jumps over the lazy dog.")
+
 	failureTests := []struct {
 		name    string
 		counter int64
@@ -66,7 +68,7 @@ func TestGenerate(t *testing.T) {
 			name:    "test_negative_input",
 			counter: -1,
 			digits:  6,
-			secret:  toBase32("The quick brown fox jumps over the lazy dog."),
+			secret:  sharedSecret,
 			hasher:  sha512.New,
 		},
 		{
@@ -90,7 +92,7 @@ func TestGenerate(t *testing.T) {
 			name:           "test_otp_1_10_digits",
 			counter:        54324343,
 			digits:         10,
-			secret:         toBase32("The quick brown fox jumps over the lazy dog."),
+			secret:         sharedSecret,
 			hasher:         sha512.New,
 			expectedOutput: "0582933009",
 		},
@@ -98,7 +100,7 @@ func TestGenerate(t *testing.T) {
 			name:           "test_otp_2_6_digits",
 			counter:        54324351,
 			digits:         6,
-			secret:         toBase32("The quick brown fox jumps over the lazy dog."),
+			secret:         sharedSecret,
 			hasher:         sha512.New,
 			expectedOutput: "934368",
 		},
@@ -106,7 +108,7 @@ func TestGenerate(t *testing.T) {
 			name:           "test_otp_3_sha256_hasher",
 			counter:        54324354,
 			digits:         6,
-			secret:         toBase32("The quick brown fox jumps over the lazy dog."),
+			secret:         sharedSecret,
 			hasher:         sha256.New,
 			expectedOutput: "181011",
 		},
@@ -114,7 +116,7 @@ func TestGenerate(t *testing.T) {
 			name:           "test_otp_4_60_period",
 			counter:        27162206,
 			digits:         10,
-			secret:         toBase32("The quick brown fox jumps over the lazy dog."),
+			secret:         sharedSecret,
 			hasher:         sha512.New,
 			expectedOutput: "1796746380",
 		},
@@ -138,6 +140,128 @@ func TestGenerate(t *testing.T) {
 
 			if *otp != tt.expectedOutput {
 				t.Errorf("OTP and the expected output are not the same! Got: %v, expected: %v!", *otp, tt.expectedOutput)
+			}
+		})
+	}
+}
+
+func TestVerify(t *testing.T) {
+	sharedSecret := toBase32("The quick brown fox jumps over the lazy dog.")
+	period := 30
+
+	errorTests := []struct {
+		name    string
+		otp     string
+		counter int64
+		digits  int
+		secret  string
+		hasher  func() hash.Hash
+	}{
+		{
+			name:    "test_otp_not_enough_length",
+			otp:     "1234",
+			counter: 1629780615 / int64(period),
+			digits:  10,
+			secret:  sharedSecret,
+			hasher:  sha512.New,
+		},
+		{
+			name:    "test_otp_fail_counter",
+			otp:     "000000",
+			counter: -1,
+			digits:  6,
+			secret:  sharedSecret,
+			hasher:  sha512.New,
+		},
+	}
+
+	invalidTests := []struct {
+		name            string
+		otp             string
+		verifyTimestamp int64
+		digits          int
+		secret          string
+		hasher          func() hash.Hash
+	}{
+		{
+			name:            "test_otp_invalid_because_more_than_30_seconds",
+			otp:             "1736605286", // OTP generated at 1629787611.
+			verifyTimestamp: 1629787651,   // sometimes, it can be valid for a bit more than 30 seconds.
+			digits:          10,
+			secret:          sharedSecret,
+			hasher:          sha512.New,
+		},
+		{
+			name:            "test_otp_all_zeroes",
+			otp:             "000000",
+			verifyTimestamp: 1629787651, // taken from the above.
+			digits:          6,
+			secret:          sharedSecret,
+			hasher:          sha512.New,
+		},
+		{
+			name:            "test_otp_before_timestamp",
+			otp:             "1736605286", // OTP generated at 1629787611.
+			verifyTimestamp: 1629787555,   // 56 seconds before OTP generation time
+			digits:          10,
+			secret:          sharedSecret,
+			hasher:          sha512.New,
+		},
+	}
+
+	successTests := []struct {
+		name            string
+		otp             string
+		verifyTimestamp int64
+		digits          int
+		secret          string
+		hasher          func() hash.Hash
+	}{
+		{
+			name:            "test_otp_5_seconds",
+			otp:             "1941056642", // OTP generated at 1629787672
+			verifyTimestamp: 1629787677,
+			digits:          10,
+			secret:          sharedSecret,
+			hasher:          sha512.New,
+		},
+	}
+
+	for _, tt := range errorTests {
+		t.Run(tt.name, func(t *testing.T) {
+			valid, err := Verify(&tt.otp, tt.counter, tt.digits, tt.secret, tt.hasher)
+			if err == nil {
+				t.Errorf("Test case should return an error! Got: %v, expected: %v!", err, valid)
+			}
+		})
+	}
+
+	for _, tt := range invalidTests {
+		t.Run(tt.name, func(t *testing.T) {
+			verifyCounter := tt.verifyTimestamp / int64(period)
+
+			valid, err := Verify(&tt.otp, verifyCounter, tt.digits, tt.secret, tt.hasher)
+			if err != nil {
+				t.Errorf("Verification test-cases should not return error(s)! Got: %v!", err)
+			}
+
+			if valid {
+				t.Errorf("Result of the test-cases should be invalid. Got: %v!", valid)
+			}
+		})
+	}
+
+	for _, tt := range successTests {
+		t.Run(tt.name, func(t *testing.T) {
+			verifyCounter := tt.verifyTimestamp / int64(period)
+
+			valid, err := Verify(&tt.otp, verifyCounter, tt.digits, tt.secret, tt.hasher)
+			if err != nil {
+				t.Errorf("Verification test-cases should not return error(s)! Got: %v!", err)
+			}
+
+			if !valid {
+				t.Errorf("Result of the test-cases should be valid. Got: %v!", valid)
 			}
 		})
 	}
