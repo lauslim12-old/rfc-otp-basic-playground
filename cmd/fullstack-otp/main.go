b1613a8f6ade724354f3e51d53091ae78634cc33
lauslim12 <nicholasdwiarto@yahoo.com>
2021-08-24 16:12:48 +0700

    refactor: Refactor code to use structs

--- cmd/fullstack-otp/main.go
+++ cmd/fullstack-otp/main.go
@@ -13,28 +13,35 @@ func main() {
 	// OTP metrics.
 	// Reference: https://datatracker.ietf.org/doc/html/rfc6238.
 	sharedSecret := base32.StdEncoding.EncodeToString([]byte("The quick brown fox jumps over the lazy dog."))
-	period := 30
-	digits := 10
-	currentTime := time.Now().Unix()
-	counter := currentTime / int64(period)
-	hashFunction := sha512.New
+	totpConfig := otp.TOTPConfig{
+		Secret:    sharedSecret,
+		Period:    30,
+		Digits:    10,
+		Timestamp: time.Now().Unix(),
+		Hasher:    sha512.New,
+	}
 
 	// Create OTP.
-	token, err := otp.Generate(counter, digits, sharedSecret, hashFunction)
+	token, err := otp.Generate(totpConfig)
 	if err != nil {
 		panic(err)
 	}
 
 	// Get my OTP.
 	success := fmt.Sprintf(
-		"My OTP is '%s', with shared secret '%s', with the counter being '%d' and current time being '%d'.", *token, sharedSecret, counter, currentTime,
+		"My OTP is '%s', with shared secret '%s', with current time being '%d'.", token, sharedSecret, totpConfig.Timestamp,
 	)
 	fmt.Println(success)
 
 	// Verify my OTP.
-	currentTimeMore := time.Now().Add(30*time.Second).Unix() / int64(period)
-
-	res, err := otp.Verify(token, currentTimeMore, digits, sharedSecret, hashFunction)
+	res, err := otp.Verify(token, otp.TOTPValidateConfig{
+		Secret:    sharedSecret,
+		Period:    30,
+		Digits:    10,
+		Timestamp: time.Now().Unix(),
+		Hasher:    sha512.New,
+		Window:    1,
+	})
 	if err != nil {
 		panic(err)
 	}
