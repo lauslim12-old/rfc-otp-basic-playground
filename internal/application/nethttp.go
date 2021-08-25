45fed8ed5342a2da47c14432abd00cec358bd192
lauslim12 <nicholasdwiarto@yahoo.com>
2021-08-25 10:58:19 +0700

    feat: Implement baseline webserver

---
+++ internal/application/nethttp.go
@@ -0,0 +1,103 @@
+package application
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+)
+
+// SuccessResponse is used to handle successful requests.
+type SuccessResponse struct {
+	Status  string `json:"status"`
+	Code    int    `json:"code"`
+	Message string `json:"message"`
+}
+
+// NewSuccessResponse is used to create a default, new success response.
+func NewSuccessResponse(code int, message string) *SuccessResponse {
+	return &SuccessResponse{
+		Status:  "success",
+		Code:    code,
+		Message: message,
+	}
+}
+
+// FailureResponse is used to handle failed requests.
+type FailureResponse struct {
+	Status  string `json:"status"`
+	Code    int    `json:"code"`
+	Message string `json:"message"`
+}
+
+// NewFailureResponse is used to create a default, new failure response.
+func NewFailureResponse(code int, message string) *FailureResponse {
+	return &FailureResponse{
+		Status:  "fail",
+		Code:    code,
+		Message: message,
+	}
+}
+
+// Utility function to send succesful response.
+func sendSuccessResponse(w http.ResponseWriter, successResponse *SuccessResponse) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(successResponse.Code)
+	json.NewEncoder(w).Encode(successResponse)
+}
+
+// Utility function to send failure response.
+func sendFailureResponse(w http.ResponseWriter, failureResponse *FailureResponse) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(failureResponse.Code)
+	json.NewEncoder(w).Encode(failureResponse)
+}
+
+// Configure is used to configure the application (server is initialized in 'main').
+func Configure() http.Handler {
+	// Create a Chi instance.
+	r := chi.NewRouter()
+
+	// Set up Chi's natural middlewares.
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	// Set up custom middleware.
+	r.Use(func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Add("X-Application-Name", "Fullstack OTP")
+			w.Header().Add("Server", "net/http")
+			next.ServeHTTP(w, r)
+		})
+	})
+
+	// Group routes.
+	r.Route("/api/v1", func(r chi.Router) {
+		// Sample GET request.
+		r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+			res := NewSuccessResponse(http.StatusOK, "Welcome to 'net/http' API!")
+			sendSuccessResponse(w, res)
+		})
+
+		// Declare method not allowed as a fallback.
+		r.MethodNotAllowed(func(w http.ResponseWriter, r *http.Request) {
+			errorMessage := fmt.Sprintf("Method '%s' is not allowed in this route!", r.Method)
+			res := NewFailureResponse(http.StatusMethodNotAllowed, errorMessage)
+			sendFailureResponse(w, res)
+		})
+
+		// Declare 404 every time a request reaches here.
+		r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+			errorMessage := fmt.Sprintf("Route '%s' with method '%s' does not exist in this server!", r.RequestURI, r.Method)
+			res := NewFailureResponse(http.StatusNotFound, errorMessage)
+			sendFailureResponse(w, res)
+		})
+	})
+
+	// Return our configured infrastructure.
+	return r
+}
