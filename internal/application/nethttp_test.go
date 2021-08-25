352144c0c9892d38ab6e32b0586fb632372dee26
lauslim12 <nicholasdwiarto@yahoo.com>
2021-08-25 12:16:59 +0700

    test: Add tests for JSON decoder

--- internal/application/nethttp_test.go
+++ internal/application/nethttp_test.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -84,6 +85,107 @@ func TestGeneralHandlers(t *testing.T) {
 			assert.NotNil(t, w.Body)
 			assert.Equal(t, tt.expectedStatus, w.Code)
 			assert.JSONEq(t, structToJSON(tt.expectedBody), w.Body.String())
+		})
+	}
+}
+
+func TestDecodeJSONBody(t *testing.T) {
+	handler := Configure()
+	ts := httptest.NewServer(handler)
+	defer ts.Close()
+
+	tests := []struct {
+		name         string
+		method       string
+		route        string
+		input        string
+		expectedBody *FailureResponse
+		withHeader   bool
+	}{
+		{
+			name:         "test_content_type",
+			method:       http.MethodPost,
+			route:        "/api/v1",
+			input:        `{"username":"kaede","password":"kaede"}`,
+			expectedBody: NewFailureResponse(http.StatusUnsupportedMediaType, "The 'Content-Type' header is not 'application/json'!"),
+			withHeader:   false,
+		},
+		{
+			name:         "test_json_bad_format",
+			method:       http.MethodPost,
+			route:        "/api/v1",
+			input:        `{"username":"kaede","password":"kaede",examplebadinput}`,
+			expectedBody: NewFailureResponse(http.StatusBadRequest, "Request body contains a badly formatted JSON at position 40!"),
+			withHeader:   true,
+		},
+		{
+			name:         "test_json_unexpected_eof",
+			method:       http.MethodPost,
+			route:        "/api/v1",
+			input:        `{"username":"kaede","password":"kaede"`,
+			expectedBody: NewFailureResponse(http.StatusBadRequest, "Request body contains a badly-formed JSON!"),
+			withHeader:   true,
+		},
+		{
+			name:         "test_json_wrong_data_type",
+			method:       http.MethodPost,
+			route:        "/api/v1",
+			input:        `{"username":"kaede","password":1234}`,
+			expectedBody: NewFailureResponse(http.StatusBadRequest, "Request body contains an invalid value for the \"password\" field at position 35!"),
+			withHeader:   true,
+		},
+		{
+			name:         "test_json_unknown_field",
+			method:       http.MethodPost,
+			route:        "/api/v1",
+			input:        `{"username":"kaede","password":"kaede","unknownAttribute":"1234"}`,
+			expectedBody: NewFailureResponse(http.StatusBadRequest, "Request body contains unknown field '\"unknownAttribute\"'!"),
+			withHeader:   true,
+		},
+		{
+			name:         "test_json_empty",
+			method:       http.MethodPost,
+			route:        "/api/v1",
+			input:        "",
+			expectedBody: NewFailureResponse(http.StatusBadRequest, "Request body must not be empty!"),
+			withHeader:   true,
+		},
+		{
+			name:         "test_json_too_large",
+			method:       http.MethodPost,
+			route:        "/api/v1",
+			input:        `{"username":"kaede","password":"kaede","password":"kaede","password":"kaede","password":"kaede","password":"kaede","password":"kaede","password":"kaede","password":"kaede","password":"kaede","password":"kaede","password":"kaede","password":"kaede","password":"kaede","password":"kaede","password":"kaede","password":"kaede","password":"kaede","password":"kaede","password":"kaede","password":"kaede","password":"kaede","password":"kaede","password":"kaede","password":"kaede","password":"kaede","password":"kaede","password":"kaede"}`,
+			expectedBody: NewFailureResponse(http.StatusRequestEntityTooLarge, "Request body must not be larger than 512 bytes!"),
+			withHeader:   true,
+		},
+		{
+			name:         "test_double_json",
+			method:       http.MethodPost,
+			route:        "/api/v1",
+			input:        "{}{}",
+			expectedBody: NewFailureResponse(http.StatusBadRequest, "Request body must only contain a single JSON object!"),
+			withHeader:   true,
+		},
+		{
+			name:         "test_success",
+			method:       http.MethodPost,
+			route:        "/api/v1",
+			input:        `{"username":"kaede","password":"kaede"}`,
+			expectedBody: nil,
+			withHeader:   true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			r := httptest.NewRequest(tt.method, tt.route, strings.NewReader(tt.input))
+			w := httptest.NewRecorder()
+			if tt.withHeader {
+				r.Header.Set("Content-Type", "application/json")
+			}
+
+			failureResponse := decodeJSONBody(w, r, &AuthRequestBody{})
+			assert.JSONEq(t, structToJSON(tt.expectedBody), structToJSON(failureResponse))
 		})
 	}
 }
