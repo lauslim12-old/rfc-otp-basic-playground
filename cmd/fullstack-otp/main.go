d120987843b95a2504adb6f1108fc17a85512511
lauslim12 <nicholasdwiarto@yahoo.com>
2021-08-25 10:57:58 +0700

    feat: Change OTP main to webserver main

--- cmd/fullstack-otp/main.go
+++ cmd/fullstack-otp/main.go
@@ -1,52 +1,70 @@
 package main
 
 import (
-	"crypto/sha512"
-	"encoding/base32"
+	"context"
 	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
-	"github.com/lauslim12/fullstack-otp/internal/otp"
+	"github.com/lauslim12/fullstack-otp/internal/application"
 )
 
+// Get port from environment variable. If it does not exist, use '8080'.
+func getPort() string {
+	port := os.Getenv("PORT")
+	if port == "" {
+		return ":8080"
+	}
+
+	return fmt.Sprintf(":%s", port)
+}
+
+// Starting point, initialize server.
 func main() {
-	// OTP metrics.
-	// Reference: https://datatracker.ietf.org/doc/html/rfc6238.
-	sharedSecret := base32.StdEncoding.EncodeToString([]byte("The quick brown fox jumps over the lazy dog."))
-	totpConfig := otp.TOTPConfig{
-		Secret:    sharedSecret,
-		Period:    30,
-		Digits:    10,
-		Timestamp: time.Now().Unix(),
-		Hasher:    sha512.New,
+	// HTTP server initialization.
+	server := &http.Server{Addr: getPort(), Handler: application.Configure()}
+
+	// Prepare context for graceful shutdown.
+	serverCtx, serverStopCtx := context.WithCancel(context.Background())
+
+	// Listen for syscall signals for process to interrupt or quit.
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+	go func() {
+		<-sig
+
+		// Shutdown signal with grace period of 30 seconds.
+		shutdownCtx, shutdownCtxCancel := context.WithTimeout(serverCtx, 30*time.Second)
+		defer shutdownCtxCancel()
+		log.Println("Server starting to shutdown in 30 seconds...")
+
+		go func() {
+			<-shutdownCtx.Done()
+			if shutdownCtx.Err() == context.DeadlineExceeded {
+				log.Fatal("Graceful shutdown timeout, forcing exit.")
+			}
+		}()
+
+		// Trigger graceful shutdown here.
+		server.SetKeepAlivesEnabled(false)
+		if err := server.Shutdown(shutdownCtx); err != nil {
+			log.Fatalf("Could not gracefully shutdown the server: %v\n", err)
+		}
+		serverStopCtx()
+	}()
+
+	// Run our server and print out starting message.
+	log.Printf("Server has started on port %s!", getPort())
+	err := server.ListenAndServe()
+	if err != nil && err != http.ErrServerClosed {
+		log.Fatal(err)
 	}
 
-	// Create OTP.
-	token, err := otp.Generate(totpConfig)
-	if err != nil {
-		panic(err)
-	}
-
-	// Get my OTP.
-	success := fmt.Sprintf(
-		"My OTP is '%s', with shared secret '%s', with current time being '%d'.", token, sharedSecret, totpConfig.Timestamp,
-	)
-	fmt.Println(success)
-
-	// Verify my OTP.
-	res, err := otp.Verify(token, otp.TOTPValidateConfig{
-		Secret:    sharedSecret,
-		Period:    30,
-		Digits:    10,
-		Timestamp: time.Now().Unix(),
-		Hasher:    sha512.New,
-		Window:    1,
-	})
-	if err != nil {
-		panic(err)
-	}
-
-	// Get result.
-	validOTP := fmt.Sprintf("My OTP validity is %v.", res)
-	fmt.Println(validOTP)
+	// Wait for server context to be stopped.
+	<-serverCtx.Done()
+	log.Println("Server shut down successfully!")
 }
