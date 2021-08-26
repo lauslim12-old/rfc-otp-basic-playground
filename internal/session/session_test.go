fbf05601dd643b3e0d9387e341699e9921c6f522
lauslim12 <nicholasdwiarto@yahoo.com>
2021-08-26 15:48:52 +0700

    test: Improve test suite to get all sessions

--- internal/session/session_test.go
+++ internal/session/session_test.go
@@ -3,6 +3,7 @@ package session
 
 import (
 	"errors"
+	"fmt"
 	"log"
 	"testing"
 	"time"
@@ -17,22 +18,23 @@ var sessionExpiration = time.Minute * 15
 func TestSet(t *testing.T) {
 	rdb, mock := redismock.NewClientMock()
 	service := New(rdb, sessionExpiration)
-	sessionKey, err := GenerateSessionID(32)
+	sessionID, err := GenerateSessionID(32)
 	if err != nil {
 		log.Fatal(err.Error())
 	}
+	sessionKey := fmt.Sprintf("sess:%s", sessionID)
 
 	t.Run("test_set_key_success", func(t *testing.T) {
 		mock.ExpectSet(sessionKey, "randomUser", sessionExpiration).SetVal("")
 
-		err := service.Set(sessionKey, "randomUser")
+		err := service.Set(sessionID, "randomUser")
 		assert.Equal(t, nil, err)
 	})
 
 	t.Run("test_set_key_failure", func(t *testing.T) {
 		mock.ExpectSet(sessionKey, "randomUser", sessionExpiration).SetErr(errors.New("Expect an error!"))
 
-		err := service.Set(sessionKey, "randomUser")
+		err := service.Set(sessionID, "randomUser")
 		assert.NotNil(t, err)
 	})
 }
@@ -40,15 +42,16 @@ func TestSet(t *testing.T) {
 func TestGet(t *testing.T) {
 	rdb, mock := redismock.NewClientMock()
 	service := New(rdb, sessionExpiration)
-	sessionKey, err := GenerateSessionID(32)
+	sessionID, err := GenerateSessionID(32)
 	if err != nil {
 		log.Fatal(err.Error())
 	}
+	sessionKey := fmt.Sprintf("sess:%s", sessionID)
 
 	t.Run("test_get_key_success", func(t *testing.T) {
 		mock.ExpectGet(sessionKey).SetVal("randomUser")
 
-		res, err := service.Get(sessionKey)
+		res, err := service.Get(sessionID)
 		if err != nil {
 			log.Fatal(err.Error())
 		}
@@ -59,7 +62,7 @@ func TestGet(t *testing.T) {
 	t.Run("test_get_key_fail_nil", func(t *testing.T) {
 		mock.ExpectGet(sessionKey).RedisNil()
 
-		res, err := service.Get(sessionKey)
+		res, err := service.Get(sessionID)
 		if err != nil {
 			log.Fatal(err.Error())
 		}
@@ -70,7 +73,39 @@ func TestGet(t *testing.T) {
 	t.Run("test_get_key_fail_err", func(t *testing.T) {
 		mock.ExpectGet(sessionKey).SetErr(errors.New("Expect an error!"))
 
-		_, err := service.Get(sessionKey)
+		_, err := service.Get(sessionID)
 		assert.Equal(t, "Expect an error!", err.Error())
+	})
+}
+
+func TestAll(t *testing.T) {
+	rdb, mock := redismock.NewClientMock()
+	service := New(rdb, sessionExpiration)
+
+	t.Run("test_get_keys_null", func(t *testing.T) {
+		mock.ExpectScan(0, "sess:*", 10).RedisNil()
+
+		res, err := service.All()
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+
+		assert.Equal(t, []KeyAndUser(nil), res)
+	})
+
+	t.Run("test_get_keys_success", func(t *testing.T) {
+		expectedOutput := []KeyAndUser{{"sess:1", "mock-user"}, {"sess:2", "mock-user"}}
+		mockSessions := []string{"sess:1", "sess:2"}
+
+		mock.ExpectScan(0, "sess:*", 10).SetVal(mockSessions, 0)
+		mock.ExpectGet("sess:1").SetVal("mock-user")
+		mock.ExpectGet("sess:2").SetVal("mock-user")
+
+		res, err := service.All()
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+
+		assert.Equal(t, expectedOutput, res)
 	})
 }
